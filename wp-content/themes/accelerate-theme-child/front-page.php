09b2c4453a0a5a2bf630cb85dbae601bf9b27e46
s-underwood785 <swood785@gmail.com>
2020-02-04 21:27:24 -0800

    added menu items to header, fixed Twitter module

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -58,6 +58,7 @@ get_header(); ?>
   		</div>
 			<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
 				<div id="secondary" class="widget-area" role="complementary">
+					<h4>Recent Tweets</h4>
 					<?php dynamic_sidebar( 'sidebar-2' ); ?>
 					<p><a href="https://twitter.com/skillcrush">Follow Us</a></p>
 				</div>
